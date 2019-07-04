281025e85fed8c3293ffb2c3d3ae0a75cd1f23af
Felipe Cardoso <felipe.cardoso@serpro.gov.br>
2019-07-04 17:54:33 -0300

    Remoção de print para debug

--- app/mod_auth/api/__init__.py
+++ app/mod_auth/api/__init__.py
@@ -11,7 +11,6 @@ def marshal_paginate(function):
         if "page" in data.keys():
             for k in ["curr", "prev", "next", "last"]:
                 if k in data["page"].keys() and data["page"][k]:
-                    print("RESULTTTTT", data["page"][k])
                     data["page"][k] = parse.urljoin(request.base_url, str(data["page"][k]))
         return data
     return wrapper
